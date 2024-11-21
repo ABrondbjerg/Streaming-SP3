e69a51d95f1be087f1ca1a1500880385a02a3337
Alex Brøndbjerg <alex.brondbjerg@persgroep.net>
2024-11-21 21:27:11 +0100

    Basics

--- src/FileIO.java
+++ src/FileIO.java
@@ -1,8 +1,28 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class FileIO {
 
     public void saveUserData{}
 
-    public void loadUserData(){}
+    public ArrayList<String> loadUserData(String path){
+        ArrayList<String> data = new ArrayList();
+        File file = new File(path);
+        try {
+            Scanner scan = new Scanner(file);
+            scan.nextLine();//skip header
+
+            while (scan.hasNextLine()) {
+                String line = scan.nextLine(); // "tess, 40000"
+                data.add(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File was not found");
+        }
+        return data;
+    }
 
     public void loadMovieData(){}
 }
